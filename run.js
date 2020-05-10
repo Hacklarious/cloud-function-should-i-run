87a9abb2ff3a1bd5156611ec026ef2fa2d43afbb
Matiss Janis Aboltins <matiss@mja.lv>
2020-05-10 13:28:46 +0100

    Update run.js

--- run.js
+++ run.js
@@ -3,5 +3,7 @@ function getRandomInt(max) {
 }
 
 exports.run = (req, res) => {
+  res.set('Access-Control-Allow-Origin', "*");
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
   res.status(200).send(getRandomInt(100) > 70);
 };
